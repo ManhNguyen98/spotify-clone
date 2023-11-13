86224d69c4908c992167368cf8901b0ccd687d7c
Manh Nguyen <nguyenthietmanh@gapo.com.vn>
2023-11-13 17:38:31 +0700

    Initial project

---
+++ providers/UserProvider.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { MyUserProvider } from '@/hooks/useUser'
+
+interface UserProviderProps {
+  children: React.ReactNode
+}
+
+const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+  return <MyUserProvider>{children}</MyUserProvider>
+}
+
+export default UserProvider
